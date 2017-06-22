399132f6e2276d9b654b3721c02382e2d862c108
Jayden Kenny <kennyjay@umail.iu.edu>
2017-06-21 23:00:50 -0400

    CSS for Sign In is beautiful

--- src/SignIn.js
+++ src/SignIn.js
@@ -11,13 +11,23 @@ const SignIn = () =>{
     }
     return(
         <div>
-         <button className="SignIn" onClick={authenticate}>
-            Github
-            </button>
-            <button className="SignIn" onClick={authenticateFacebook}>
-            Facebook
-            </button>
+            <h1 className="welcome">Welcome to Noteherder</h1>
+            <div className="pls">
+                <p>Got something on your mind?
+                    Want somewhere to store your random thoughts?
+                    Like keeping lists? 
+                    <br />
+                    <br />
+                    <i>We'd like to Welcome you Noteherder</i>
+                </p>
             </div>
+            <img src="https://fleep.io/blog/wp-content/uploads/2014/07/github_icon.png" className="SignIn" onClick={authenticate} />
+                    {/*Github
+                </button>*/}
+                <img src="http://centerlyne.com/wp-content/uploads/2016/07/logo-facebook.png" className="SignIn2" onClick={authenticateFacebook} />
+                    {/*Facebook
+                </button>*/}
+        </div>
     )
 
 }
